770232ea899984fd9f846f2c6194f89de1d83e88
panbi7 <panbi7@naver.com>
2024-12-19 14:23:19 +0900

    [add] css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import axios from 'axios';
+import './css/Home.css'; // CSS 파일 가져오기
 
 function Home() {
     const [date, setDate] = useState(new Date());
